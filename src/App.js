0416d8872282079e5fe9bef0b285ffb9fd648532
Yew Kang Wei <yewyew6933@gmail.com>
2020-06-03 09:27:34 +0800

    standard bmi

--- src/App.js
+++ src/App.js
@@ -1,10 +1,53 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
 
 function App() {
+  const [height, setHeight] = useState(0);
+  const [weight, setWeight] = useState(0);
+  const [result, setResult] = useState(0);
+
+  function onChangeHeight(e) {
+    setHeight(e.target.value);
+  }
+  function onChangeWeight(e) {
+    setWeight(e.target.value);
+  }
+  function onSubmitResult(e) {
+    e.preventDefault();
+    const newResult = weight / ((height / 100) ^ 2);
+    setResult(newResult);
+  }
+
   return (
     <div>
-      <h1>Hello World</h1>
+      <h1>BMI Calculator</h1>
+
+      <form onSubmit={onSubmitResult}>
+        <div className="form-group">
+          <label htmlFor="height">Your Height(cm):</label>
+          <input
+            type="number"
+            name="height"
+            value={height}
+            onChange={onChangeHeight}
+          />
+        </div>
+
+        <div className="form-group">
+          <label htmlFor="weight">Your Weight(kg):</label>
+          <input
+            type="number"
+            name="weight"
+            value={weight}
+            onChange={onChangeWeight}
+          />
+        </div>
+
+        <div className="form-group">
+          <button type="submit">Calculate</button>
+        </div>
+      </form>
+      <h1>Your Result:{result}</h1>
     </div>
   );
 }
